566236a28d1df3aad1b817c521f5d7ccb0d31a1c
Kyoya Yamaguchi <kyoryu32@gmail.com>
2018-09-25 21:25:04 +0900

    Firebaseテスト

--- MIF/ViewController/AppDelegate.swift
+++ MIF/ViewController/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,7 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = TopViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        FIRApp.configure()
         return true
     }
 }
--- MIF/ViewController/TopViewController.swift
+++ MIF/ViewController/TopViewController.swift
@@ -1,9 +1,17 @@
 import UIKit
+import Firebase
 
 final class TopViewController: UIViewController {
 
+    var ref: FIRDatabaseReference!
+    
     override func viewDidLoad() {
-        
+        ref = FIRDatabase.database().reference()
+        let S2Vote = ref.child("S2-vote")
+        let aVote = S2Vote.child("a")
+        S2Vote.observe(.value, with: { snapshot in
+            print(snapshot.value as! [String : Int])
+        })
     }
 }
 
